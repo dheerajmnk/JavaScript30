4fa7f8d40632da81a187e75783cae399aed842dc
Dheeraj Menon <dheerajmenonk@gmail.com>
2022-02-28 17:22:39 +0530

    Add Day 21-30

---
+++ follow-along-links/script.js
@@ -0,0 +1,21 @@
+const triggers = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function highlightLink(){
+    const linkCoords = this.getBoundingClientRect();
+    console.log(linkCoords);
+    const coords = {
+        width: linkCoords.width,
+        height: linkCoords.height,
+        top: linkCoords.top + scrollY,
+        left: linkCoords.left + scrollX
+    };
+
+    highlight.style.width = `${coords.width}px`;
+    highlight.style.height = `${coords.height}px`;
+    highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+}
+
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
